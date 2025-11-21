fe92443484bf10b2fd679da0221836c27a25906b
Seo-JeongJin <tj0wjd9wls8@gmail.com>
2025-11-21 23:43:25 +0900

    todo input

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+
+<template>
+  <div class="todo-container">
+    <h1>TODO List</h1>
+    
+    <!-- TodoInput 자식 컴포넌트에서 'todoInput' 이벤트가 emit되었을 때
+    부모 컴포넌트의 todoInput 메서드를 실행하라 (자식으로부터 넘어온 이벤트는 케밥 케이스로 작성되어야 함) -->
+    <TodoInput @todo-input="todoInput" />
+    <TodoList />
+   
+  </div>
+</template>
+
+<script>
+  import TodoInput from './components/TodoInput.vue';
+  import TodoList from './components/TodoList.vue';
+  import { v4 as uuidv4 } from 'uuid';
+
+  export default {
+    data() {
+      return {
+        todoList : []
+      }
+    },
+
+    components : {
+      TodoInput,
+      TodoList
+    },
+    
+    methods : {
+      todoInput(msg) { // 자식이 보낸 this.msg 값을 사용하기 위해 인자를 받음(인자 이름은 하위 컴포넌트와 맞출 필요 당연히 없음)
+        const item = {
+          id : uuidv4(),
+          msg : msg,
+          completed : false
+        }
+        this.todoList.push(item);
+        console.log(JSON.stringify(this.todoList));
+      }
+    }
+  }
+</script>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,34 @@
+
+<!-- 내가 이해한 대로 동작한 과정 설명
+사용자가 input 태그에 메세지를 입력하면 v-model 로 인하여 양방향 바인딩이 되기에 msg에 사용자가 값을 넣는대로 msg 값이 즉시 바뀜. 
+메세지를 입력하고 사용자가 버튼을 클릭하면 todoInput 메서드 실행됨. 
+todoInput 메서드 실행으로 인해 todoInput 이라는 이벤트가 this.msg 값을 함께 가지고 부모 컴포넌트로 전달됨. 
+그 후에 바로 msg 값은 초기화 됨. -->
+
+<template>
+  <form id="todo-form">
+    <input type="text" id="todo-input" 
+    placeholder="Enter a new task" v-model="msg">
+    <button type="button" @click="todoInput">Add</button>
+  </form>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        msg : '',
+      }
+    },
+    methods : {
+      todoInput() {
+        this.$emit("todoInput", this.msg); // todoInput 이벤트를 this.msg 와 함께 부모 컴포넌트로 전달
+        this.msg = '';
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
